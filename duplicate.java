9f25720c65187a7c096fdf911c256ba8b01df86c
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-28 15:30:19 +0530

    to find duplicate

---
+++ duplicate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class duplicate{	
+	public static void main (String[]args){
+		int arr[]={2,4,3,2,6};
+		for(int i=0;i<arr.length;i++){
+		    for(int j=i+1;j<arr.length;j++){
+			if(arr[i]==arr[j]){
+				System.out.print("duplicate number is:"+arr[i]);
+				break;
+	}
+}
+}
+}
+}
